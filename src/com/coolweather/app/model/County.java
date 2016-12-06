c42855b3b1834dc5b0144106ebdd16786d93b82f
liye <liye@gmail.com>
2016-12-06 20:55:58 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ src/com/coolweather/app/model/County.java
@@ -0,0 +1,34 @@
+package com.coolweather.app.model;
+
+public class County {
+
+	private int id;
+	private String countyName;
+	private String cuuntyCodel;
+	private int cityId;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCountyName() {
+		return countyName;
+	}
+	public void setCountyName(String countyName) {
+		this.countyName = countyName;
+	}
+	public String getCuuntyCodel() {
+		return cuuntyCodel;
+	}
+	public void setCuuntyCodel(String cuuntyCodel) {
+		this.cuuntyCodel = cuuntyCodel;
+	}
+	public int getCityId() {
+		return cityId;
+	}
+	public void setCityId(int cityId) {
+		this.cityId = cityId;
+	}
+	
+}
---
+++ src/com/coolweather/app/model/Province.java
@@ -0,0 +1,28 @@
+package com.coolweather.app.model;
+
+public class Province {
+
+	private int id;
+	private String provinceName;
+	private String procinceCode;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getProvinceName() {
+		return provinceName;
+	}
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+	public String getProcinceCode() {
+		return procinceCode;
+	}
+	public void setProcinceCode(String procinceCode) {
+		this.procinceCode = procinceCode;
+	}
+	
+}
